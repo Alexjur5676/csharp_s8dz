fabb2ceb58e2346fd9496a9bb73a2c42f9aa28f7
Alexjur5676 <alex9992142@mail.ru>
2022-08-30 05:13:57 +0700

    Add task54

--- task54/Program.cs
+++ task54/Program.cs
@@ -9,3 +9,61 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 9)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите m и n через Enter "); 
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
+for (int i = 0; i < matrix.GetLength(0); i++) // Array Sorting
+{
+    for (int j = matrix.GetLength(1) - 1; j > 0; j--)
+    {
+        for (int k = 0; k < j; k++)
+        {
+            if (matrix[i, k] > matrix[i, k + 1])
+            {
+                int temp = matrix[i, k];
+                matrix[i, k] = matrix[i, k + 1];
+                matrix[i, k + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
