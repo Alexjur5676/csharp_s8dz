83f2b0ed2572b47f7fd2bb1992a6e8e2b3c077a2
Alexjur5676 <alex9992142@mail.ru>
2022-08-30 14:06:29 +0700

    Add task_56_58

--- task54/Program.cs
+++ task54/Program.cs
@@ -9,12 +9,10 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
-int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 9)
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 10)
 {
     int[,] matrix = new int[rowsCount, columnsCount];
-
     Random rand = new Random();
-
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
@@ -22,7 +20,6 @@ int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightR
             matrix[i, j] = rand.Next(leftRange, rightRange + 1);
         }
     }
-
     return matrix;
 }
 void PrintMatrix(int[,] matrix)
@@ -40,14 +37,16 @@ Console.Write("Введите m и n через Enter ");
 int m = Convert.ToInt32(Console.ReadLine());
 int n = Convert.ToInt32(Console.ReadLine());
 int[,] matrix = FillMatrix(m, n);
+Console.WriteLine("Исходный массив:");
 PrintMatrix(matrix);
-for (int i = 0; i < matrix.GetLength(0); i++) // Array Sorting
+
+for (int i = 0; i < matrix.GetLength(0); i++) 
 {
     for (int j = matrix.GetLength(1) - 1; j > 0; j--)
     {
         for (int k = 0; k < j; k++)
         {
-            if (matrix[i, k] > matrix[i, k + 1])
+            if (matrix[i, k] < matrix[i, k + 1])
             {
                 int temp = matrix[i, k];
                 matrix[i, k] = matrix[i, k + 1];
@@ -56,14 +55,9 @@ for (int i = 0; i < matrix.GetLength(0); i++) // Array Sorting
         }
     }
 }
+
 Console.WriteLine();
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        Console.Write(matrix[i, j] + " ");
-    }
-    Console.WriteLine();
-}
+Console.WriteLine("Отсортированный массив:");
+PrintMatrix(matrix);
 
 
--- task56/Program.cs
+++ task56/Program.cs
@@ -10,3 +10,50 @@
 // Программа считает сумму элементов в каждой 
 // строке и выдаёт номер строки с наименьшей 
 // суммой элементов: 1 строка
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите m и n через Enter "); 
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrix(m, n);
+Console.WriteLine("Исходный массив:");
+PrintMatrix(matrix);
+
+int minSum = 0;
+int iMinSum = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++) 
+    {
+        sum = sum + matrix[i, j];
+    } 
+    if (sum < minSum || i == 0)
+    {
+        minSum = sum;
+        iMinSum = i+1;
+    }
+}
+Console.WriteLine($"Наименьшая сумма элементов в {iMinSum} строке");
